106570cb38a83cdb2c2a68b4aeed9cd23d67801e
humberto junior <astronomi@gmail.com>
2022-06-18 01:00:45 -0300

    Apply icons in toolBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { AppBar, CssBaseline, ThemeProvider, Toolbar, Typography } from "@mui/material";
+import { AppBar, CssBaseline, IconButton, ThemeProvider, Toolbar, Typography } from "@mui/material";
 import theme from "./theme";
+import DriveEtaIcon from "@mui/icons-material/DriveEta";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline/>{/* <=== apply background all app */}
 
-      <AppBar position="static">
+      <AppBar position="static" enableColorOnDark>
         <Toolbar>
+          <IconButton color="inherit">
+            <DriveEtaIcon/>
+          </IconButton>
           <Typography variant="h5">Full Cycle Delivery</Typography>
         </Toolbar>
       </AppBar>
